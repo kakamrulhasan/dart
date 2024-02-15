db44dc8ea73a203716a50590994ff75ff18100bc
Kamrul <kakamrul2000@gmail.com>
2024-02-16 01:54:32 +0600

    first commit

---
+++ mydart.dart
@@ -0,0 +1,46 @@
+void main() {
+  // var my = ' baby';
+  // var myb = "baby";
+  // print(myb);
+  // print(my);
+  // var x = 10.4;
+  // var y = 3;
+  // var z = x + y;
+  // print(z);
+  //var city = ['dhaka', 'comilla', 'rajshahi'];
+  // print(city);
+  // print(city[1]);
+  // var student = {'name': 'arif', 'age': 23};
+  // print(student);
+  // print(student['name']);
+  // var a, b;
+  // a = 10;
+  // b = 4;
+  // print(a++);
+
+  // print(a);
+  //final  thisyear = 2020;
+  // thisyear = 2030;
+  // thisyear = 2040;
+  // print(thisyear);
+
+  //print(city.isNotEmpty);
+  // print(city);`
+  // city.add("cox'sBAZAR");
+  // print(city);
+  // city.add('noua');
+  // print(city);
+  //var number = [1, 2, 3, 4, 5];
+  // number.add(9);
+  // number.add(10);
+  // number.addAll([11, 34]);
+  // print(number);
+
+  // number.insert(3, 22);
+  // print(number);
+  // number.insertAll(2, [133, 32, 34343, 234]);
+  // print(number);
+  // number.remove(3);
+  // print(number);
+  var myCityset = {'dhaka', ' barisal', ' coxs bazar'};
+}
