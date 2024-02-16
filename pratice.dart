dbcc42c79af805c20001e5e0abd2188ce511ba4e
Kamrul <kakamrul2000@gmail.com>
2024-02-17 00:31:01 +0600

    add

--- pratice.dart
+++ pratice.dart
@@ -1,5 +1,9 @@
 void main(List<String> args) {
+  // dart list
   var city = ['dhaka', 'rangpur', 'comilla'];
   print(city);
-  print(city[0]); // dart list
+  print(city[0]);
+  //dart Maps
+  var student = {'name':'josh', "age": "23"};
+  print(student["name"]);
 }
