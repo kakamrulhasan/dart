dadbd7376a59c5890126179b3dc4e3f187e5adfd
Kamrul <kakamrul2000@gmail.com>
2024-02-17 00:39:08 +0600

    add

--- pratice.dart
+++ pratice.dart
@@ -1,9 +1,25 @@
 void main(List<String> args) {
   // dart list
   var city = ['dhaka', 'rangpur', 'comilla'];
-  print(city);
-  print(city[0]);
+  // print(city);
+  // print(city[0]);
   //dart Maps
-  var student = {'name':'josh', "age": "23"};
+  var student = {'name': 'josh', "age": "23"};
   print(student["name"]);
+
+  //Fixed Length List
+  print(city);
+  city.add("khulna");
+  print(city);
+  city.add("noyakhali");
+  print(city);
+
+  //insert List
+  var number = [1, 2, 3, 4];
+  number.insert(2, 299);
+  print(number);
+
+  //insert List
+  number.insertAll(3, [100, 200, 2003, 470]);
+  print(number);
 }
