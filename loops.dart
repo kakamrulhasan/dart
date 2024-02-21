e83d682e7e9f9f4cd3de641f2c181144ddfa6bc5
Kamrul <kakamrul2000@gmail.com>
2024-02-22 01:58:38 +0600

    add

--- loops.dart
+++ loops.dart
@@ -2,6 +2,14 @@ void main(List<String> args) {
   // for (var i = 0; i < 100; i= i+2) {
   //   print("dart: "+i.toString());
   // }
+
+
   //DART FOR.... IN LOOP OVER LIST
+  // var Alphalist = ['A', 'B', 'C', 'D', 'E'];
+  // for (var Alphabet in Alphalist) {
+  //   print(Alphabet);
+  // }
+
+  //For .... IN LOOP OVER MAP(JSON ARRAY)
   
 }
