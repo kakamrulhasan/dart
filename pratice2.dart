9f38283fef7b887862c1e4604444b53d866911d7
Kamrul <kakamrul2000@gmail.com>
2024-02-22 01:32:43 +0600

    add

--- pratice2.dart
+++ pratice2.dart
@@ -6,16 +6,30 @@ void main() {
   // print("joy");
   // print("dont't know");
 
-  int marks = 9;
-  if (marks >= 80) {
-    print("RESULT is A+");
-  } else if (marks < 80 && marks >= 70) {
-    print("result is A");
-  } else if (marks < 70 && marks >= 60) {
-    print("result is A-");
-  } else if (marks < 60 && marks >= 50) {
-    print("result is B");
-  } else {
-    print("Failed");
+  // int marks = 9;
+  // if (marks >= 80) {
+  //   print("RESULT is A+");
+  // } else if (marks < 80 && marks >= 70) {
+  //   print("result is A");
+  // } else if (marks < 70 && marks >= 60) {
+  //   print("result is A-");
+  // } else if (marks < 60 && marks >= 50) {
+  //   print("result is B");
+  // } else {
+  //   print("Failed");
+  // }
+  var marks = 71;
+  switch (marks) {
+    case 90:
+      print("A");
+      break;
+    case 80:
+      print("B");
+      break;
+    case 70:
+      print("C");
+      break;
+    default:
+      print("result not found");
   }
 }
