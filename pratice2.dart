c0d30fda896ab8f5b3186741a34dc7a77a8306dc
Kamrul <kakamrul2000@gmail.com>
2024-02-22 01:22:49 +0600

    add

---
+++ pratice2.dart
@@ -0,0 +1,21 @@
+void main() {
+  // String myString = 'hello';
+  // print(myString);
+  // print(myString.contains('hello baby'));
+  // print("joy bangla");
+  // print("joy");
+  // print("dont't know");
+
+  int marks = 9;
+  if (marks >= 80) {
+    print("RESULT is A+");
+  } else if (marks < 80 && marks >= 70) {
+    print("result is A");
+  } else if (marks < 70 && marks >= 60) {
+    print("result is A-");
+  } else if (marks < 60 && marks >= 50) {
+    print("result is B");
+  } else {
+    print("Failed");
+  }
+}
